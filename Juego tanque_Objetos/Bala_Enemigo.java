89863e38c4bdb87de794d6615eb170869ca4da6a
fitellez <dante_92_007@hotmail.com>
2016-04-30 01:15:46 -0500

    Avance de proyecto
    Esta definido el movimiento del jugador; al momento de encontrar una
    pared este no avance. El disparo del jugador. El destruir enemigos

---
+++ Juego tanque_Objetos/Bala_Enemigo.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bala_Enemigo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bala_Enemigo extends MoverObjetos
+{
+    /**
+     * Act - do whatever the Bala_Enemigo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
